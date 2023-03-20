764ff72750783447631796e2ba5ebc1a162dab59
Kamini-C <iamkaminichhabra@gmail.com>
2023-03-20 17:32:25 -0400

    Personal Info Component completed

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalInformationComponent implements OnInit {
 
+  myInfo: string[][] = [
+    ['Name', 'Kamini Chhabra'],
+    ['DOB', 'Dec 21,1991'],
+    ['Work Experience', '5+ Years'],
+    ['Education', 'MSc. Computer Science'],
+    ['Interests', 'Pencil Sketching'],
+  ];
+
+  aboutMe: string[] = [
+    'Ambitious and solution-driven Web Developer adept at contributing to highly collaborative work environment, finding solutions and determining customer satisfaction. Proven experience in developing consumer-focused websites. ',
+    'Built products for Web users, meeting highest standards for web design, user experience, best practices and speed. Constantly seeking growth and learning experiences.'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
